2812f1cb249f82f57d67c81cbd7912485ed77271
bulton-fr <bulton.fr@gmail.com>
2019-10-03 16:59:03 +0200

    Add german language
    Thanks to TNTKokser

---
+++ lang/de.lua
@@ -0,0 +1,14 @@
+-- GERMAN LANGUAGE LOCALIZATION
+
+-- Cardinal points
+ZO_CreateStringId("SI_LIB_DRAGON_WORLD_EVENT_CP_NORTH", "Norden")
+ZO_CreateStringId("SI_LIB_DRAGON_WORLD_EVENT_CP_SOUTH", "Süden  ")
+ZO_CreateStringId("SI_LIB_DRAGON_WORLD_EVENT_CP_WEST",  "Westen")
+
+-- Dragon status
+ZO_CreateStringId("SI_LIB_DRAGON_WORLD_EVENT_STATUS_FIGHT",   "Im Kampf")
+ZO_CreateStringId("SI_LIB_DRAGON_WORLD_EVENT_STATUS_KILLED",  "Getötet")
+ZO_CreateStringId("SI_LIB_DRAGON_WORLD_EVENT_STATUS_UNKNOWN", "Unbekannt")
+ZO_CreateStringId("SI_LIB_DRAGON_WORLD_EVENT_STATUS_WAITING", "Wartet")
+ZO_CreateStringId("SI_LIB_DRAGON_WORLD_EVENT_STATUS_FLYING",  "Fliegt")
+ZO_CreateStringId("SI_LIB_DRAGON_WORLD_EVENT_STATUS_WEAK",    "Im Kampf (Leben < 50%)")
